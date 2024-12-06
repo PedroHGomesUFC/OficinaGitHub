f73359690b85111b5374f1ea925d3b202a9e098f
DaviFcnd <davisfacundo12@gmail.com>
2024-12-06 14:19:12 -0300

    adicionando soma em java

---
+++ sla.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class sla {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int soma = a + b;
+        System.out.println(soma);
+        sc.close();
+    }
+    
+}
